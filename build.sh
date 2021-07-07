0dddd205c1545099b0ad5a64656476331ea50bdf
Zoë Marschner <zoe.marschner@gmail.com>
2021-07-07 16:44:17 -0400

    update dates, dynamic data

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd website/src/py
+python3 update_date.py
 python3 staticPageGenerator.py
 cd ../../..
 echo "Finished running python files."
--- website/src/py/update_date.py
+++ website/src/py/update_date.py
@@ -1,6 +1,19 @@
-files = ['projectTemplate.html', '../../index.html']
+from datetime import datetime
 
+now = datetime.now()
+
+files = ['projectTemplate.html', '../../index.html']
+new_contents = [];
+now_str = now.strftime("%B %Y")
 
 for file in files:
 	with open(file, 'r') as f:
-		contents = file.read()
+		contents = f.read()
+		ind = contents.find("©")
+		ind2 = contents.find('<',ind)
+		new = contents[:ind+2] + now_str + contents[ind2:]
+		new_contents.append(new)
+
+for file, content in zip(files, new_contents):
+	with open(file, 'w') as f:
+		f.write(content)
