7c46c1f15e066ef91b8951a93555fc419b1b5456
Zoë Marschner <zoe.marschner@gmail.com>
2023-08-04 02:59:01 +0200

    actually fix url encoding for umlauts

--- website/src/js/picturePicker.js
+++ website/src/js/picturePicker.js
@@ -6,7 +6,6 @@ window.addEventListener('load', function init() {
 	  .then(function(json) {
 	  	image_list = json;
 	  	chosen_image = image_list[Math.floor(Math.random()*image_list.length)];
-	  	console.log(chosen_image);
 
 	  	title = chosen_image.substr(0, chosen_image.indexOf('.'));
 	  	extra_info_ind = title.indexOf("_");
@@ -14,7 +13,7 @@ window.addEventListener('load', function init() {
 
 	  	portrait = document.getElementById("portrait");
 	  	portrait.title = title;
-	  	portrait.src = "public/img/spotlight_pictures/" + chosen_image;
+	  	portrait.src = "public/img/spotlight_pictures/" + encodeURIComponent(chosen_image);
 
 	  });
 
