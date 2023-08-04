f39ab64117e71b4ea1d9fb689449c030ffaa7b6f
Zoë Marschner <zoe.marschner@gmail.com>
2023-08-04 09:30:14 +0200

    fix url encoding issue AGAIN (change combining characters to non-combining characters)

--- website/src/py/build_image_manifest.py
+++ website/src/py/build_image_manifest.py
@@ -8,5 +8,19 @@ imgdir = "../../public/img/spotlight_pictures"
 os.chdir(imgdir)
 data = [file for file in glob.glob("*.JPG") + glob.glob("*.jpeg") + glob.glob("*.jpg")]
 
+
+# change combining characters --> non combining, 
+# seems to be required to get encoding of uri in live version to work
+u_char = {'a': '\u00e4', 'o': '\u00f6', 'u': '\u00fc'}
+reform_data = []
+for file in data:
+    reform_file = ""
+    for i in range(len(file)-1):
+        if file[i+1] == '̈':
+            reform_file += u_char[file[i]]
+        elif file[i] != '̈':
+            reform_file += file[i]
+    reform_data.append(reform_file + file[-1])
+
 with open(f'manifest.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(reform_data, f)
