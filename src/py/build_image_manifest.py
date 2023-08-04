ccd4520460397f28e197537e890af438a3e1a770
Zoë Marschner <zoe.marschner@gmail.com>
2023-08-04 02:59:01 +0200

    actually fix url encoding for umlauts

--- src/py/build_image_manifest.py
+++ src/py/build_image_manifest.py
@@ -8,13 +8,5 @@ imgdir = "../../public/img/spotlight_pictures"
 os.chdir(imgdir)
 data = [file for file in glob.glob("*.JPG") + glob.glob("*.jpeg") + glob.glob("*.jpg")]
 
-# fix unicode encoding errors with umlauts
-enc_data = []
-for file in data:
-    mappings = [(u"ä", "%C3%A4"), (u"ö", "%C3%B6"), (u"ü", "%C3%BC")]
-    for charmap in mappings:
-        file = file.replace(*charmap)
-    enc_data.append(file)
-
 with open(f'manifest.json', 'w') as f:
-    json.dump(enc_data, f)
+    json.dump(data, f)
